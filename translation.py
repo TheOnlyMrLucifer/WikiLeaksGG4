dba47462c3b8f0de96aced3cdbebd41c085462d2
Chloe Decker <69217587+TheOnlyMrLucifer@users.noreply.github.com>
2022-06-29 09:46:51 +0200

    v1
    v1

---
+++ translation.py
@@ -0,0 +1,12 @@
+class Translation(object):
+
+      # Start text
+      START = "Hi I am a channel auto forward bot \n\n<b>Owner and GateWay:</b>\n @TheOnlyMrLucifer, @WikiLeaksGateWay"             
+
+      #About text
+      ABOUT = """
+<b>📝 Language:</b> Python 3
+<b>🧰 Framework:</b> Pyrogram
+<b>👨‍💻 Developer:</b> WikiLeaks @TheOnlyMrLucifer
+<b>📢 Channel:</b> WikiLeaksGateWay
+<b>👥 Groups:</b> Heaven's
